a1c21ef9b47e51fcf9602ea6ad1edc03cda9ff70
malcomowilla <owillamalcom@gmail.com>
2023-02-20 22:21:58 +0300

    super

---
+++ lib/student.rb
@@ -0,0 +1,25 @@
+# Student
+# #hello
+# returns a greeting phrase
+# #raise_hand
+# returns the phrase 'Pick me!'
+
+
+
+
+
+
+
+class Student
+    
+
+    def hello
+     puts   "Hey there! I'm so excited to learn stuff."
+    end
+
+    def raise_hand
+       puts "Pick me!"
+    end
+
+    
+end
